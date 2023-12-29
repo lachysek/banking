3146ccfba9af28a196868edaa687ec73c431bb79
lachysek <127331280+lachysek@users.noreply.github.com>
2023-12-29 22:28:45 +0100

    Create request.lua

---
+++ request.lua
@@ -0,0 +1,5 @@
+local resourceName = GetCurrentResourceName()
+    if resourceName ~= 'hnl_banking' then
+        print('^1 What are you trying do rn? :)' .. resourceName.. ' need to be named hnl_banking :)')
+        break
+    end
